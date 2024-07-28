b57089d770202f9c9feaf1378c1c548ccb31c9dc
prakhartiwari0 <65062036+prakhartiwari0@users.noreply.github.com>
2024-07-28 20:16:30 +0530

    faqs icon issue

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 
-// import crossBarImgURL from './assets/Cross Bar@4x.png'
 
+import crossBarImgURL from './assets/Cross Bar@4x.png'
 
 
 function toggleFaq(element) {
@@ -9,10 +9,30 @@ function toggleFaq(element) {
 
     if (answer.classList.contains('hidden')) {
         answer.classList.remove('hidden');
-        // icon.src = crossBarImgURL;
+        icon.src = crossBarImgURL;
         icon.src = 'assets/Cross Bar@4x.png';
     } else {
         answer.classList.add('hidden');
         icon.src = 'assets/Drop Plus@4x.png';
     }
 }
+
+
+let faqElements = document.querySelectorAll('.faq_item')
+
+faqElements.forEach(element=>{
+    element.addEventListener('click', ()=>{
+        const answer = element.nextElementSibling;
+        const icon = element.querySelector('.faq_icon');
+        
+        if (answer.classList.contains('hidden')) {
+            answer.classList.remove('hidden');
+            icon.src = crossBarImgURL;
+            // icon.src = 'assets/Cross Bar@4x.png';
+        } else {
+            answer.classList.add('hidden');
+            icon.src = 'assets/Drop Plus@4x.png';
+        }
+        
+    })
+})
