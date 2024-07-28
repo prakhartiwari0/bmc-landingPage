f230e3d67cdd8c91c33867328b64c9a81a93b1e7
prakhartiwari0 <65062036+prakhartiwari0@users.noreply.github.com>
2024-07-28 23:24:28 +0530

    vite fix huh

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     root: 'src',
-    assetsInclude:'src/**/*.html'
+    // assetsInclude:'src/**/*.html'
     // other configurations...
 })
