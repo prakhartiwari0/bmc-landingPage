2457daf7032979e17cc547fc8708147dc74b237b
prakhartiwari0 <65062036+prakhartiwari0@users.noreply.github.com>
2024-07-29 08:37:43 +0530

    make responsive and other changes

---
+++ src/pages/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const otpInputs = document.querySelectorAll(".otp_input");
+
+    otpInputs.forEach((input, index) => {
+        input.addEventListener("input", () => {
+            if (input.value.length === 1 && index < otpInputs.length - 1) {
+                otpInputs[index + 1].focus();
+            }
+        });
+
+        input.addEventListener("keydown", (e) => {
+            if (e.key === "Backspace" && input.value === "" && index > 0) {
+                otpInputs[index - 1].focus();
+            }
+        });
+    });
+});
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     root: 'src',
+    build:'vite build --base=./'
     // assetsInclude:'src/**/*.html'
     // other configurations...
 })
